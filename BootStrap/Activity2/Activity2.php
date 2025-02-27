966edc87091d63e212eb0dcff9ae503b6b498f2e
Luis Joshua Bulatao <bluisjoshua@gmail.com>
2025-02-27 23:46:35 +0800

    Add files via upload

---
+++ BootStrap/Activity2/Activity2.php
@@ -0,0 +1,198 @@
+<html lang="en">
+
+<head>
+    <title>BootStrap Activity2</title>
+</head>
+
+<body>
+    <h1>Activity2</h1>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+
+        <!-- Badges and Label Class -->
+        <div class="container-fluid">
+
+            <a href="#">Messages<span class="badge bg-secondary">10</span></a><br/>
+            <a href="#">Notifications<span class="badge bg-secondary">30</span></a>
+            <button class="btn btn-primary">Click to see notifications<span class="badge bg-secondary">500</span></button>
+            
+            <div class="page-header">
+                <h3>Download <span class="tag tag-default">Version10.1</span></h3>
+            </div>
+        </div>
+
+
+
+    <br/>
+    <br/>
+    <hr/>
+
+        <!-- Alert Class -->
+    <div class="container-fluid">
+        <div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert">&times;</a>Record already Exist!!!</div>
+        <div class="alert alert-success"><a href="#" class="close" data-dismiss="alert">&times;</a>Record already Exist!!!</div>
+        <div class="alert alert-warning"><a href="#" class="close" data-dismiss="alert">&times;</a>Record already Exist!!!</div>
+        <div class="alert alert-info"><a href="#" class="close" data-dismiss="alert">&times;</a>Record already Exist!!!</div>
+    </div>
+
+    <br/>
+    <br/>
+    <hr/>
+
+    <!-- Page Header and ell Class -->
+    <div class="container-fluid">
+        <div class="jumbotron">
+            <h1>Heading in Jumbotron Class</h1>
+
+        </div>
+            <h1>Heading in Without Jumbotron Class</h1>
+
+        <div class="page-header">
+            <h1>Welcome</h1>
+
+        </div>
+
+        <p class="well well-sm">This is the well</p>
+        <p class="well">This is the well</p>
+        <p class="well well-lg">This is the well</p>
+    </div>
+
+
+    <br/>
+    <br/>
+    <hr/>
+
+    <!-- GLPYICON -->
+    <div class="container-fluid">
+        <span class="glyphicon glyphicon-trash"></span>:Trash Can <br/>
+        <span class="glyphicon glyphicon-pencil"></span>:Pencil <br/>
+        <button class="btn btn-success"><span class="glyphicon glyphicon-search "></span>Find</button>
+    </div>
+    
+
+
+
+    <br/>
+    <br/>
+    <hr/>
+
+    <!-- Dropdown Classess -->
+    <div class="container-fluid">
+        <div class="dropdown">
+            <button class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Colleges <span class="caret"></span></button>
+            <ul class="dropdown-menu">
+                <li><a href="#">CSS</li>
+                <li><a href="#">CET</li>
+                <li><a href="#">CBA</li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Exit</li>
+                <li><a href="#">CSS</a></li>
+            </ul>
+        </div>
+    </div>
+
+        <div class="dropdown">
+            <button class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Colleges <span class="caret"></span></button>
+            <ul class="dropdown-menu dropdown-menu-right">
+                <li><a href="#">CSS</li>
+                <li><a href="#">CET</li>
+                <li><a href="#">CBA</li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Exit</li>
+                <li><a href="#">CSS</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="dropup">
+            <button class="btn btn-primary dropdown-toggle" data-toggle="dropdown">Colleges <span class="caret"></span></button>
+            <ul class="dropdown-menu">
+                <li><a href="#">CSS</li>
+                <li><a href="#">CET</li>
+                <li><a href="#">CBA</li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Exit</li>
+                <li><a href="#">CSS</a></li>
+            </ul>
+        </div>
+    </div>
+
+
+
+
+
+    <br/>
+    <br/>
+    <hr/>
+
+    <!-- Button and Classes -->
+    <div class="container-fluid">
+        <button class="btn btn-default">Click Me</button>
+        <button class="btn btn-primary btn-xs">Click Me</button>
+        <button class="btn btn-info btn-lg">Click Me</button>
+        <button class="btn btn-success btn-md">Click Me</button>
+        <button class="btn btn-warning">Click Me</button>
+        <button class="btn btn-danger btn-sm">Click Me</button>
+        <button class="btn btn-link">Click Me</button>
+        <button class="btn btn-danger btn-block">Click Me</button>
+        <button class="btn btn-danger disabled">Click Me</button>
+        <button class="btn btn-danger active">Click Me</button>
+
+
+        <div class="btn-group">
+            <button class="btn btn-info">Home</button>
+            <button class="btn btn-info">About Us</button>
+            <button class="btn btn-info">Contact Us</button>
+            <button class="btn btn-info">Product</button>
+        </div>
+
+        <div class="btn-group-vertical">
+            <button class="btn btn-info">Home</button>
+            <button class="btn btn-info">About Us</button>
+            <button class="btn btn-info">Contact Us</button>
+            <button class="btn btn-info">Product</button>
+        </div>
+
+        <div class="btn-group btn-group-justified">
+            <div class="btn-group">
+                <button class="btn btn-success">Home</button>
+            </div>
+
+            <div class="btn-group">
+                <button class="btn btn-success">About Us</button>
+            </div>
+
+            <div class="btn-group">
+                <button class="btn btn-success">Contact Us</button>
+            </div>
+
+            <div class="btn-group">
+                <button class="btn btn-success">Products</button>
+            </div>
+      
+        </div>
+
+
+
+
+    </div>
+    
+
+    
+     <hr/>
+     <br/>   
+    <!-- Image and Classes -->
+    <div class="container-fluid">
+        <img src="image/Bulatao, Luis Joshua D..png">
+        <img src="image/Bulatao, Luis Joshua D..png" class="img-rounded">
+        <img src="image/Bulatao, Luis Joshua D..png" class="img-circle">
+        <img src="image/Bulatao, Luis Joshua D..png" class="img-thumbnail">
+
+
+
+    </div>
+</body>
+
+</html> 
